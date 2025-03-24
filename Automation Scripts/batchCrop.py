fc4f3342c690038b7caaf2c21544f0b5a5ff2648
ElleLiagre <elle.liagre@u-bordeaux.fr>
2025-03-24 18:13:29 +0100

    Update batchCrop.py
    Addition of header to script.

--- Automation Scripts/batchCrop.py
+++ Automation Scripts/batchCrop.py
@@ -1,3 +1,28 @@
+# ===========================================================================
+# Title: A Standardized, Three-Dimensional Cropping Protocol for Analyzing 
+#        the Medial Epicondyle of the Humerus - Script for Batch Cropping
+# 
+# Author: Elle B. K. Liagre
+# Email: elle.liagre@u-bordeaux.fr
+# ORCID: https://orcid.org/0000-0002-8993-3266
+# Version: 1.0
+# Date: 2025-03-24
+# 
+# Description: Batch function to crop all 3D models in a specified folder. If 
+# no folder path is provided, a window will prompt you to select one. Cropped 
+# models will be saved in a new folder within the original folder. Note: This 
+# function is for right humeri only due to orientation requirements. If run 
+# for a left-sided model, the humeral diaphysis will be extracted instead
+# of the medial epicondyle. More information can be found on: 
+# https://github.com/ElleLiagre/medial-epicondyle-cropping-protocol
+#
+# Software Requirements: 
+# - 3D Slicer (https://www.slicer.org/)
+#
+# License: This script is licensed under the GNU General Public License v3.0.
+# See https://www.gnu.org/licenses/gpl-3.0.html for more details.
+# ===========================================================================  
+
 import slicer
 import vtk
 import os
